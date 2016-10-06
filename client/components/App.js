e8b4d891c44f287d04fa10e17fc3c8c5179d8af9
danilichev <v.danilichev@gmail.com>
2016-10-06 20:19:49 +0300

    Added basic transition

--- client/components/App.js
+++ client/components/App.js
@@ -1,30 +1,44 @@
 
-import React from 'react';  
-import FlyoutSidebar from './FlyoutSidebar';
+import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import FlyoutSidebar from './FlyoutSidebar.js';
 import loremIpsum from '../data/lorem-ipsum.js';
+
+import './App.less';
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			isFlyoutOpen: false
+			items: []
 		}
 
-		this.openFlyout = this.openFlyout.bind(this);
+		this.addItem = this.addItem.bind(this);
 	}  
 
-	openFlyout() {
-		this.refs.flyout.changeOpenState();
+	addItem() {
+		const newItems = this.state.items.concat('Sidebar'); 
+		this.setState({
+			items: newItems
+		})
 	}
 
   render() {
+  	const items = this.state.items.map((item, i) => (
+  		<FlyoutSidebar key={i} sidebarName={item}>{loremIpsum}</FlyoutSidebar>
+  	));
+
     return (
     	<div>
-    		<button onClick={this.openFlyout}>Open Flyout</button>
-    		<FlyoutSidebar ref="flyout" isOpen={this.state.isFlyoutOpen} sidebarName="firstSidebar">
-    			{loremIpsum}
-    		</FlyoutSidebar>
+    		<button onClick={this.addItem}>Add item</button>
+    		<ReactCSSTransitionGroup
+      		transitionName="example"
+      		transitionEnterTimeout={500}
+      		transitionLeaveTimeout={500}
+      	>
+      		{items}	
+      	</ReactCSSTransitionGroup>
     	</div>
     );
   }
--- client/components/FlyoutSidebar.js
+++ client/components/FlyoutSidebar.js
@@ -1,41 +1,14 @@
 import React from 'react';
 
-class FlyoutSidebar extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			isOpen: props.isOpen
-		};
-
-		this.changeOpenState = this.changeOpenState.bind(this);
-		this.getClassName = this.getClassName.bind(this);
-	}	
-
-	changeOpenState() {
-		this.setState({
-			isOpen: !this.state.isOpen
-		});
-	}
-
-	getClassName() {
-		return `flyout-sidebar ${this.state.isOpen ? "open" : "closed"}`;
-	}
-
-	render() {
-		return (
-			<div className={this.getClassName()}>
-				<div 
-					className="flyout-sidebar-header"
-					onClick={this.changeOpenState}>
-					{this.props.sidebarName}
-				</div>
-				<div className="flyout-sidebar-content">
-					{this.props.children}
-				</div>
-			</div>
-		);
-	}
-};
+const FlyoutSidebar = (props) => (
+	<div className="flyout-sidebar">
+		<div className="flyout-sidebar-header">
+			{props.sidebarName}
+		</div>
+		<div className="flyout-sidebar-content">
+			{props.children}
+		</div>
+	</div>
+); 
 
 export default FlyoutSidebar;
