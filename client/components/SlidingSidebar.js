d0459ed565eeda3047a8a9581ce5174eeaf3b6d9
danilichev <v.danilichev@gmail.com>
2016-10-09 23:08:09 +0300

    Little refactoring of SlidingSidebar

--- client/components/SlidingSidebar.js
+++ client/components/SlidingSidebar.js
@@ -7,61 +7,67 @@ class SlidingSidebar extends React.Component {
 	constructor(props) {
 		super(props);
 
+		this.TOGGLE_SIDEBAR = 'toggle-sidebar';
+		this.TOGGLE_SLIDE_NEXT = 'toggle-slide-next';
+		this.TOGGLE_SLIDE_PREV = 'toggle-slide-prev';
+
 		this.state = {
+			transition: this.TOGGLE_SIDEBAR,
 			slides: []
 		}
 
+		this._setSlide = this._setSlide.bind(this);
 		this.openSidebar = this.openSidebar.bind(this);
 		this.closeSidebar = this.closeSidebar.bind(this);
-		this.getCurrentSlide = this.getCurrentSlide.bind(this);
+		this.getLastSlide = this.getLastSlide.bind(this);
 		this.setNextSlide = this.setNextSlide.bind(this);
 		this.setPrevSlide = this.setPrevSlide.bind(this);
 	}
 
-	openSidebar(slide) {
-		const { title, content } = slide;
+	_setSlide(slide, transition) {
+		const slides = this.state.slides;
+		const nextSlide = { 
+			_id: slides.length,
+			title: slide.title, 
+			content: slide.content
+		};
 
 		this.setState({
-			slides: [{
-				_id: 0,
-				title,
-				content
-			}]
+			transition,
+			slides: slides.concat(nextSlide)
 		});
 	}
 
-	closeSidebar() {
-		this.setState({slides: []});
+	openSidebar(slide) {
+		this._setSlide(slide, this.TOGGLE_SIDEBAR);
 	}
 
-	getCurrentSlide() {
+	closeSidebar() {
+		const transition = this.TOGGLE_SIDEBAR;
+		this.setState({
+			slides: [],
+			transition
+		});
+	}
+
+	getLastSlide() {
 		const slides = this.state.slides;
 		return slides.length > 0 ? slides[slides.length - 1] : null;
 	}
 
 	setNextSlide(slide) {
-		const { title, content } = slide;
-		const slides = this.state.slides;
-		const nextSlide = { 
-			_id: slides.length,
-			title, 
-			content 
-		};
-
-		this.setState({
-			slides: slides.concat(nextSlide)
-		});
+		this._setSlide(slide, this.TOGGLE_SLIDE_NEXT);
 	}
 
 	setPrevSlide() {}
 
 	render() {
-		const slide = this.getCurrentSlide();
+		const slide = this.getLastSlide();
 
 		return (
 			<div className="flyout-sidebar">
 				<ReactCSSTransitionGroup 
-          transitionName="sidebar-toggling" 
+          transitionName={this.state.transition} 
           transitionEnterTimeout={500} 
           transitionLeaveTimeout={500}>
           {slide ? 
