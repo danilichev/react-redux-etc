f75c89b6d1e5c6d4950ce076a25cf2929aaa9cfd
danilichev <v.danilichev@gmail.com>
2016-10-13 01:21:48 +0300

    Stuff

--- client/components/SlidingSidebar.js
+++ client/components/SlidingSidebar.js
@@ -10,10 +10,14 @@ const SlidingSidebarActions = {
   SHOW_PREV_SLIDE: 'SHOW_PREV_SLIDE'
 };
 
-const transitionConfig = {
- 	TIMEOUT: 500,
- 	TO_LEFT: 'transition-left',
- 	TO_RIGHT: 'transition-right'
+const SlidingSidebarConfig = {
+ 	TIMEOUT: 400,
+ 	transitions: {
+ 		OPEN: 'transition-open',
+ 		CLOSE: 'transition-close',
+ 		PUSH: 'transition-push',
+ 		POP: 'transition-pop'
+ 	}
 }; 
 
 class SlidingSidebar extends React.Component {
@@ -21,38 +25,41 @@ class SlidingSidebar extends React.Component {
     super(props);
 
     this.actionTypes = SlidingSidebarActions;
-    this.transition = transitionConfig;
+    this.timeout = SlidingSidebarConfig.TIMEOUT;
+    this.transitions = SlidingSidebarConfig.transitions;
 
     this.state = {
+    	isOpen: false,
     	slides: [],
-    	slideTransition: this.transition.TO_RIGHT
+    	transition: ''
     };
 
     this._pushSlide = this._pushSlide.bind(this);
     this._popSlide =this._popSlide.bind(this);
     this._getLastSlide = this._getLastSlide.bind(this);
     this._cleanSlides = this._cleanSlides.bind(this);
-    this._isSidebarOpen = this._isSidebarOpen.bind(this);
   }
 
   _pushSlide(slide) {
     const slides = this.state.slides;
     const newSlide = Object.assign(slide, { key: slides.length });
 
-    this.setState({ 
+    this.setState({
+    	isOpen: true,
     	slides: slides.concat(newSlide),
-    	slideTransition: slides.length === 0 ? this.transition.TO_RIGHT : this.transition.TO_LEFT
+    	transition: slides.length === 0 ? this.transitions.OPEN : this.transitions.PUSH
     });
   }
 
   _popSlide() {
     const slides = this.state.slides.slice();
-    slides.pop();
 
-    this.setState({ 
-    	slides,
-    	slideTransition: slides.length === 0 ? this.transition.TO_LEFT : this.transition.TO_RIGHT
-    });
+    if (slides.pop()) {
+    	this.setState({ 
+    		slides,
+    		transition: slides.length === 0 ? this.transitions.CLOSE : this.transitions.POP
+    	});
+    }    
   }
 
   _getLastSlide() {
@@ -63,46 +70,48 @@ class SlidingSidebar extends React.Component {
   _cleanSlides() {
   	this.setState({
       slides: [],
-      slideTransition: this.transition.TO_LEFT
+      transition: this.transitions.CLOSE
     });
-  }
-
-  _isSidebarOpen() {
-  	return this.state.slides.length > 0;
   }
 
   componentWillReceiveProps(nextProps) {
     const { action, nextSlide } = nextProps;
     const actions = this.actionTypes;
-    const isSidebarOpen = this._isSidebarOpen();
 
     switch(action) {
       case actions.OPEN_SIDEBAR:
-        !isSidebarOpen && this._pushSlide(nextSlide);
-        break;    
+      	!this.state.isOpen && this._pushSlide(nextSlide);    
       case actions.CLOSE_SIDEBAR:
-        isSidebarOpen && this._cleanSlides();
+        this.state.isOpen && this._cleanSlides();
         break;
       case actions.SHOW_NEXT_SLIDE:
-        isSidebarOpen && this._pushSlide(nextSlide);
-        break;
+        this._pushSlide(nextSlide);
+        break;  
       case actions.SHOW_PREV_SLIDE:
-        isSidebarOpen && this._popSlide();
+        this._popSlide();
         break;
     }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+  	console.log({prevState});
+
+  	if (prevState.transition.CLOSE) {
+  		this.setTimeout(() => this.setState({ isOpen: false}), this.timeout);
+  	}
   }
 
   render() {
     const slide = this._getLastSlide();
 
     return (
-      <div className={`sliding-sidebar ${this._isSidebarOpen() ? 'open' : 'close'}`}>
+      <div className={`sliding-sidebar ${this.state.isOpen ? 'open' : ''}`}>
         <ReactCSSTransitionGroup
         	component="div"
         	className="slider-wrapper"
-          transitionName={this.state.slideTransition} 
-          transitionEnterTimeout={this.transition.TIMEOUT} 
-          transitionLeaveTimeout={this.transition.TIMEOUT}>
+          transitionName={this.state.transition} 
+          transitionEnterTimeout={this.timeout} 
+          transitionLeaveTimeout={this.timeout}>
           {slide ? <Slide {...slide} goBack={this._popSlide}>{slide.content}</Slide> : null}
         </ReactCSSTransitionGroup>
       </div>
