ccd2277cee3ef87e65f12639acddc9bd30d75675
danilichev <v.danilichev@gmail.com>
2016-10-27 20:45:41 +0300

    Removed redundant code for start new example

--- client/components/App.js
+++ client/components/App.js
@@ -1,68 +1,15 @@
 import React from 'react';
-import { SlidingSidebar, SlidingSidebarActions } from './SlidingSidebar.js';
-import loremIpsum from '../data/lorem-ipsum.js';
-import './App.less';
+
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      action: null,
-      nextSlide: null
-    }
+  constructor(props) {
+    super(props);
   }
 
   render() {
-    const closeSidebarButton = (
-      <button onClick={() => this.setState({
-          action: SlidingSidebarActions.CLOSE_SIDEBAR
-        })}>
-        Close Sidebar
-      </button>
-    );
-
-    const getButtomToNextSlide = (nextSlide) => (
-      <button onClick={() => this.setState({
-          action: SlidingSidebarActions.SHOW_NEXT_SLIDE,
-          nextSlide
-        })}>
-        {`Go to ${nextSlide.title}`}
-      </button>
-    );
-
-    const thirdSlide = {
-      title: 'Slide 3',
-      content: closeSidebarButton
-    }
-
-    const secondSlide = {
-      title: 'Slide 2',
-      content: getButtomToNextSlide(thirdSlide)
-    }    
-
-    const firstSlide = {
-      title: 'Slide 1',
-      content: getButtomToNextSlide(secondSlide)
-    }
-
-    const openSidebarButton = (
-      <button 
-        onClick={() => this.setState({
-          action: SlidingSidebarActions.OPEN_SIDEBAR,
-          nextSlide: firstSlide
-        })}>
-        Open Sidebar
-      </button>
-    );
-
     return (
       <div className="app">
-        <div className="toggle-sidebar">
-          {openSidebarButton}
-          {closeSidebarButton}
-        </div>
-        <SlidingSidebar {...this.state}/>
+        APP
       </div>
     );
   }
--- client/index.js
+++ client/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
-import './styles/index.less';
 
-const ROOT = document.getElementById('root');
 
 ReactDOM.render(
 	<App />,
-	ROOT
+	document.getElementById('root')
 );
