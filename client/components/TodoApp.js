173cc822d0d355d586a02e4efb20da95c7df5f5c
danilichev <v.danilichev@gmail.com>
2016-10-30 20:49:39 +0200

    Extracted TodoList

--- client/components/TodoApp.js
+++ client/components/TodoApp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { store } from '../store/store';
 import { FilterLink } from './FilterLink';
+import TodoList from './TodoList';
 
 let todoId = 0;
 
@@ -33,22 +34,15 @@ class TodoApp extends Component {
         }}>
           Add Todo
         </button>
-        <ul>
-          {visibleTodos.map(todo => 
-            <li 
-              key={todo.id}
-              onClick={() => {
-                store.dispatch({
-                  type: 'TOGGLE_TODO',
-                  id: todo.id
-                });
-              }}
-              style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-            >
-              {todo.text}
-            </li>
-          )}
-        </ul>
+        <TodoList 
+          todos={visibleTodos}
+          onClickTodo={(id) => {
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id
+            });
+          }}
+        />
         <p>
           Show: 
           {' '}
---
+++ client/components/TodoList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+const Todo = ({ text, completed, onClick }) => (
+  <li
+    onClick={onClick}
+    style={{ textDecoration: completed ? 'line-through' : 'none' }}
+  >
+    {text}
+  </li>
+);
+
+const TodoList = ({ todos, onClickTodo}) => (
+  <ul>
+    {todos.map(todo => 
+      <Todo 
+        key={todo.id}
+        {...todo}
+        onClick={() => onClickTodo(todo.id)}
+      />   
+    )}
+  </ul>
+);
+
+export default TodoList;
